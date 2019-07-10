bbed9feb75510f934ef774c4058767825afa0e73
Kotodian <417350372@qq.com>
2019-07-10 13:52:52 +0800

    优化json数据

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
 ##安装Scrapy框架
-
-- scrapy start project [projectname]
-
+```python
+scrapy start project [projectname]
+```
 
 
 ##创建爬虫
-- scrapy genspider example example.com
+```python
+scrapy genspider example example.com
+```
+##获取数据
+```python
+#response是一个'scrapy.http.response.html.HtmlResponse'对象,可执行'xpath'和'css'方法获取数据。
+response.xpath()#返回selectorList
+response.xpath().xpath()#返回selector
+#getall获取全部文本返回一个列表
+
+```
+##运行命令行
+```python
+from scrapy import cmdline
+cmdline.execute(""scrapy crawl qsbk_spider".split()")
+```
+##数据解析
+```python
+#yleid 返回数据提交到pipline处理
+#pipline获得数据将其转换成json数据格式。
+
+```
+
+###JsonItemExporter和JsonLinesItemExporter:
+- 保存json数据的时候,可以使用这两个类,让操作变更简单。
--- qsbk/pipelines.py
+++ qsbk/pipelines.py
@@ -4,14 +4,62 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import json
 
+
+# class QsbkPipeline(object):
+#
+#     def __init__(self):
+#         # super(CLASS_NAME, self).__init__(*args, **kwargs)
+#         self.fp=open("duanzi.json",'w',encoding='utf-8')
+#
+#
+#
+#     def open_spider(self, spider):
+#         print('start')
+#         pass
+#
+#     def process_item(self, item, spider):
+#         item_json=json.dumps(dict(item),ensure_ascii=False)
+#         self.fp.write(item_json+'\n')
+#         return item
+#
+#     def close_spider(self,spider):
+#         self.fp.close()
+#         print('end')
+#         pass
+# import json
+# from scrapy.exporters import  JsonItemExporter,JsonLinesItemExporter
+# class QsbkPipeline(object):
+#
+#     def __init__(self):
+#         # super(CLASS_NAME, self).__init__(*args, **kwargs)
+#         #以二进制打开
+#         self.fp=open("duanzi.json",'wb')
+#         self.exporter=JsonItemExporter(self.fp,ensure_ascii=False,encoding='utf-8')
+#         self.exporter.start_exporting()
+#
+#     def open_spider(self, spider):
+#         print('start')
+#         pass
+#
+#     def process_item(self, item, spider):
+#         self.exporter.export_item(item)
+#         return item
+#
+#     def close_spider(self,spider):
+#         self.exporter.finish_exporting()
+#         self.fp.close()
+#         print('end')
+#         pass
+import json
+from scrapy.exporters import  JsonLinesItemExporter
 class QsbkPipeline(object):
 
     def __init__(self):
         # super(CLASS_NAME, self).__init__(*args, **kwargs)
-        self.fp=open("duanzi.json",'w',encoding='utf-8')
-
+        #以二进制打开
+        self.fp=open("duanzi.json",'wb')
+        self.exporter=JsonLinesItemExporter(self.fp,ensure_ascii=False,encoding='utf-8')
 
 
     def open_spider(self, spider):
@@ -19,11 +67,11 @@ class QsbkPipeline(object):
         pass
 
     def process_item(self, item, spider):
-        item_json=json.dumps(dict(item),ensure_ascii=False)
-        self.fp.write(item_json+'\n')
+        self.exporter.export_item(item)
         return item
 
     def close_spider(self,spider):
+
         self.fp.close()
         print('end')
         pass
