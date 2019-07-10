ec5525d2133c8917c6d6025cf45bd1fd1ca795e4
Kotodian <417350372@qq.com>
2019-07-10 10:47:07 +0800

    爬取糗事百科的段子

---
+++ README.md
@@ -0,0 +1,8 @@
+##安装Scrapy框架
+
+- scrapy start project [projectname]
+
+
+
+##创建爬虫
+- scrapy genspider example example.com
--- qsbk/spiders/qsbk_spider.py
+++ qsbk/spiders/qsbk_spider.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.http.response.html import HtmlResponse
+from scrapy.selector.unified import SelectorList
 
 class QsbkSpiderSpider(scrapy.Spider):
     name = 'qsbk_spider'
     allowed_domains = ['qiushibaike.com']
-    start_urls = ['http://qiushibaike.com/']
+    start_urls = ['https://www.qiushibaike.com/text/page/1/']
 
     def parse(self, response):
-        pass
+        # print(type(response))
+        #selectorList
+        duanzidivs=response.xpath("//div[@id='content-left']/div")
+        #print(type(contentleft))
+        for duanzidiv in duanzidivs:
+            #selector
+            author=duanzidiv.xpath(".//h2/text()").get().strip()
+            content=duanzidiv.xpath(".//div[@class='content']//text()").getall()
+            content="".join(content).strip()
+            print(content)
+            pass
